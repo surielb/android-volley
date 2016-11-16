679e262aa32fd25e53fcae1e7bfb7bcc88b9bc0d
Suriel Bendahan <surielb@users.noreply.github.com>
2016-11-16 22:10:29 +0200

    extended slow request

--- src/main/java/com/android/volley/toolbox/BasicNetwork.java
+++ src/main/java/com/android/volley/toolbox/BasicNetwork.java
@@ -57,7 +57,7 @@ import java.util.TreeMap;
 public class BasicNetwork implements Network {
     protected static final boolean DEBUG = VolleyLog.DEBUG;
 
-    private static int SLOW_REQUEST_THRESHOLD_MS = 3000;
+    private static int SLOW_REQUEST_THRESHOLD_MS = 15000;
 
     private static int DEFAULT_POOL_SIZE = 4096;
 
